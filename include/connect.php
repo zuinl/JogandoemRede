e894919503c639827056442acbdf8ed43fa27fe0
leonardo <leonardosoareszuin@gmail.com>
2019-09-10 19:51:18 -0300

    atualizacao

--- include/connect.php
+++ include/connect.php
@@ -2,14 +2,10 @@
     
         session_start();
     
-    $conn = mysqli_connect('servidor.taubate.sp.gov.br', 'taubates_seel', 'esportes@2017', 'taubates_seel');
+    $conn = mysqli_connect('localhost', 'root', '', 'db_avaliacao');
 
     if(!$conn) {
-        echo '
-        <script>
-            alert("Houve algum erro na conexão com o banco de dados da Secretaria de Esportes e Lazer. Tente
-            novamente mais tarde");
-        ';
+        echo 'Houve um erro ao conectar ao banco'.mysqli_error($conn);
     }
 
 ?>
